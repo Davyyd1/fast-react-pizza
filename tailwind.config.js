573b95cdf746c7169496d59d1a67dc10d6a896a7
David <d.dav889@gmail.com>
2025-02-17 17:58:41 +0200

    final app

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,20 @@ export default {
       height: {
         screen: "100dvh",
       },
+      animation: {
+        fadeIn: "fadeIn .6s ease-in-out",
+        slideIn: "slideIn 0.5s ease out",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+        slideIn: {
+          "0%": { transform: "translateX(-100%)" },
+          "100%": { transform: "translateX(0)" },
+        },
+      },
     },
   },
   plugins: [],
